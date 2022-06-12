a6aa5f8b6beecb5618779c5d58153e3480d3aed5
Olivia <ramsfol@gmail.com>
2022-06-12 17:31:33 -0500

    high score saved

--- assets/js/game.js
+++ assets/js/game.js
@@ -149,17 +149,27 @@ if (playerInfo.health > 0 && i < enemyInfo.length - 1) {
 var endGame = function() { 
     window.alert("The game has ended. Let's see how you did!");
 
-    //if player is still alive, player wins!  
-    if (playerInfo.health > 0) {
-         window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
-        } else { 
-            window.alert("You've lost your robot in battle.");
-        } 
+    // check localStorage for high score, it it is not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+
+    //if player has more money than the high score, player has the new high score!
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + highScore + "!");
+    }
+    else {
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+    }
 
         //ask player if they's like to play again  
         var playAgainConfirm = window.confirm("Would you like to play again?"); 
 
-        if (playAgainConfirm) {      
+        if (playAgainConfirm) { 
             startGame(); 
         } else { 
             window.alert("Thank you for playing Robot Gladiators! Come back soon!"); 
